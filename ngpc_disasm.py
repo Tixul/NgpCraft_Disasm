882a06ccd11b34384a6d321cfb903513b40961fb
Tixul <138982329+Tixul@users.noreply.github.com>
2026-04-01 12:49:50 +0200

    Change copyright holder to 'NgpCraft Tixu'
    Update copyright holder in the file header.

--- ngpc_disasm.py
+++ ngpc_disasm.py
@@ -4,7 +4,7 @@ ngpc_disasm.py — NGPC/NGP Disassembler
 Part of the NgpCraft open-source toolchain for Neo Geo Pocket Color.
 
 MIT License
-Copyright (c) 2026 NgpCraft contributors
+Copyright (c) 2026 NgpCraft Tixu
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
